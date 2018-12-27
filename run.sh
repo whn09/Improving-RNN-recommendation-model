4cf30fa3c91e50a8e6c86e7fcb14e6dc40a514ba
henan.wang <henan.wang@henan.wang's MacBook Pro>
2018-12-27 20:52:56 +0800

    change sceript

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-python preprocess.py -f ../dataset/log_preprocess/20181128/all_ph.txt --columns uirt --sep '\t' --val_size 1000 --test_size 5000
-python train.py -d ../dataset/log_preprocess/20181128/all_ph/
+#python preprocess.py -f ../dataset/log_preprocess/20181128/all_ph.txt --columns uirt --sep '\t' --val_size 1000 --test_size 5000
+python train.py -d ../dataset/log_preprocess/20181128/all_ph/ --log_dir logs
+#python test.py -d ../dataset/log_preprocess/20181128/all_ph/
